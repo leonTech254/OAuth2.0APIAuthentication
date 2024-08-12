40096447649d7bc6796ea5be87d35c2874e0a288
leonTech254 <martinleontech23@gmail.com>
2024-08-12 11:08:40 +0300

    [NEW] project init

---
+++ src/main/java/com/leonteqsecurity/cysec/CysecApplication.java
@@ -0,0 +1,13 @@
+package com.leonteqsecurity.cysec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CysecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CysecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonteqsecurity/cysec/CysecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonteqsecurity.cysec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CysecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
